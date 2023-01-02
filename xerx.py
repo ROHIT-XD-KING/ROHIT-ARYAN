c034b14bebfdf041aa49ee26e7223c66e5de0b16
ROHIT RAJPUT <114306698+ROHIT-XD-KING@users.noreply.github.com>
2023-01-02 12:26:04 +0530

    Add files via upload

---
+++ xerx.py
@@ -0,0 +1,10 @@
+import os
+import platform
+print("JOIN FB GROUP")
+os.system("xdg-open https://facebook.com/groups/460389902592352/")
+os.system("git pull")
+b = platform.architecture()[0]
+if b == '64bit':
+    import xerx64
+elif b == '32bit':
+    import xerx32
