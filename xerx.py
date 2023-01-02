baa768750fd781d2cc501613e88cbfe518bc259c
ROHIT RAJPUT <114306698+ROHIT-XD-KING@users.noreply.github.com>
2023-01-02 12:21:51 +0530

    Delete xerx.py

--- xerx.py
+++
@@ -1,8 +0,0 @@
-import os
-import platform
-os.system("xdg-open https://facebook.com/groups/460389902592352/")
-b = platform.architecture()[0]
-if b == '64bit':
-    import xerx64
-elif b == '32bit':
-    import xerx32
