7dd15559b3074ffd5cba572c7602daa267ed5543
ROHIT RAJPUT <114306698+ROHIT-XD-KING@users.noreply.github.com>
2023-01-06 12:30:20 +0530

    Update xerx.py

--- xerx.py
+++ xerx.py
@@ -6,6 +6,6 @@ os.system("xdg-open https://facebook.com/groups/460389902592352/")
 os.system("git pull")
 b = platform.architecture()[0]
 if b == '64bit':
-    import rohit
+    import rohit64
 elif b == '32bit':
     import rohit32
