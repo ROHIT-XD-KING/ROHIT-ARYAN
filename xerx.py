797582277c258e4fb6869df0bb8c1c32cacd8895
ROHIT RAJPUT <114306698+ROHIT-XD-KING@users.noreply.github.com>
2023-01-09 17:48:24 +0530

    Update xerx.py

--- xerx.py
+++ xerx.py
@@ -1,11 +1,4 @@
 import os
 os.system("git pull")
 os.system("clear")
-print("JOIN FB GROUP")
-os.system("xdg-open https://facebook.com/groups/460389902592352/")
-os.system("git pull")
-b = platform.architecture()[0]
-if b == '64bit':
-    import rohit64
-elif b == '32bit':
-    import rohit32
+print("\n\n HEY WE ARE CURRENTLY TURNING OFF THE COMMAND WE WILL BE BACK SOON")
